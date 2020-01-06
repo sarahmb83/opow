f9bc3c1af84842be23d48d95aa3a0e992b3e97dd
Sarah Bagwell <sarah.bagwell@finalsite.com>
2020-01-05 22:39:07 -0500

    full structure and responsive and more images

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -13,13 +13,29 @@ module.exports={
 var data = require('../data/data.json');
 //console.log(data.customer.name);
 
+//Write the first name to the H1
 var firstName = data.customer.name.split(' ').slice(0, -1).join(' ');
 $('.customer-name').html(firstName);
+
+//Write the data use to the graphs
+$('.current-year-num').html(data.energyUse.currentYear + ' kWh ');
+$('.prior-year-num').html(data.energyUse.priorYear + ' kWh ');
 
 //Handle the button active state
 $('.btn').click(function(){
 	$('.btn').removeClass('btn-active');
 	$(this).addClass('btn-active');
+
+	var test = $(this).attr('id');
+
+	if (test == 'trees') {
+		$('body').addClass('trees-active');
+		$('body').removeClass('cars-active');
+	} else {
+		$('body').addClass('cars-active');
+		$('body').removeClass('trees-active');
+	}
+
 });
 
 //module.exports = function (n) { return n * 111 };
